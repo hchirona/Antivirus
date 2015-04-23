78440c00dfbe7b48550bf468a17b4f37ebdb9328
hchirona <hectorchirona@digitalvalue.es>
2015-04-23 14:38:18 +0200

    añadidos idiomas

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$sql_server="localhost";
+$sql_user="root";
+$sql_pass="admin";
+$sql_db="db_virus";
+$cert="dv.e";
+
+        $link=mysqli_connect($sql_server, $sql_user, $sql_pass);
+        if (!$link){
+        die('Could not connect: ' . mysqli_error($link));
+        }
+
+        mysqli_select_db($link,$sql_db);
+
+
+?>
